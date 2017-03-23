1f4d6df838aaf04c47559321ae88fbf1b6abea53
Nicky van Hulst <nickyvanhulst@gmail.com>
2017-03-24 12:16:53 +1300

    Modularised title replacer

--- extension/js/title_replacer.js
+++ extension/js/title_replacer.js
@@ -1,17 +1,31 @@
-$( document ).ready(function() {
-  findAndReplaceTitle();
-});
+// Module to query for a title and replace in the page
+var titleReplacer = (()=>{
 
-// Queries extention for title for current url replaces the title if it exists
-var findAndReplaceTitle = function () {
-  // Send message to extention asking for the title
-  chrome.runtime.sendMessage({url : window.location.href}, function(response) {
-    replaceTitle(response.title);
-  });
-}
+  // Cache DOM
+  let $title = $('#eow-title');
 
-let replaceTitle = function (newTitle) {
-  if(newTitle !== ""){
-      $("#eow-title").html(newTitle);
+  // Bind events
+
+
+  // functions
+
+  // Queries extention for title for current url replaces the title if it exists
+  var findAndReplaceTitle = function () {
+    // Send message to extention asking for the title
+    chrome.runtime.sendMessage({url : window.location.href}, function(response) {
+      replaceTitle(response.title);
+    });
   }
-}
+
+  // Replaces the title on the page
+  let replaceTitle = function (newTitle) {
+    if(newTitle !== ""){
+        $title.html(newTitle);
+    }
+  }
+  return { findAndReplaceTitle : findAndReplaceTitle}
+})();
+
+$( document ).ready(function() {
+  titleReplacer.findAndReplaceTitle();
+});
