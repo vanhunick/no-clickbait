9ee4849ae15b6160a6e5023f3077f5a0fbb3dc92
Nicky van Hulst <nickyvanhulst@gmail.com>
2017-03-27 09:17:08 +1300

    Commented and removed logging from background.js

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,38 +1,36 @@
+// Run in the background of web pages relevant to the extention
+// Responds to tab changes and when a relavent pages is loaded takes in the urls on that page
 var background = (()=> {
 
   // Binding
   chrome.tabs.onUpdated.addListener(tabUpdated);
   chrome.runtime.onMessage.addListener(pageLoaded);
 
-  // functions
-
   // Called when user changes tabs checks if the extention should be activated
   function tabUpdated(id, info, tab) {
-    if (tab.url.toLowerCase().indexOf("youtube.com") > -1) {
+    if (tab.url.toLowerCase().indexOf("youtube.com") > -1) { // Any page with youtube.com at the start is valid for the extention
       chrome.pageAction.show(tab.id);
     }
   }
 
-  // Called when a page is loaded checks if the title should be replaced
+  // Called when a page is loaded and sends links on the page to database to find if titles exist for each url
   function pageLoaded(request, sender, sendResponse) {
-      if(request.urls !== undefined){
-        console.log("Multiple");
-        let paramaters = {urls : request.urls};
+      if(request.urls !== undefined){ // if true means multiple urls exist on the page
 
+        let paramaters = {urls : request.urls};
         db.findAllTitles(paramaters, (titles) => {
           if(titles.length > 0){
-            sendResponse({titles: titles}); // Return the corrosponding title to the page
+            sendResponse({titles: titles}); // Return the corrosponding title to the page (title_replacer.js)
           }
         });
-      } else {
-        console.log("Single");
+      } else { // Only one title on the page so a video page
         let paramaters = {url : request.url};
         db.findTitle(paramaters, (title) => {
-          if(title !== ""){
+          if(title !== ""){ // Check if title exists
             sendResponse({title: title}); // Return the corrosponding title to the page
           }
         });
       }
-      return true;
+      return true; // Important, makes the message syncronous
   }
 })();
